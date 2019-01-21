d9ea572bd2dfda24b51a41943180684277b67dea
Stavros Michelis <stavros.michelis@rinftech.com>
2019-01-21 09:09:46 +0200

    more

--- Rima/rima-framework/src/main/java/rima/framework/core/BasePage.java
+++ Rima/rima-framework/src/main/java/rima/framework/core/BasePage.java
@@ -9,7 +9,21 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class BasePage {
+    private static final int TIMEOUT = 5;
+    private static final int POLLING = 100;
 
+    protected WebDriver driver;
+    private WebDriverWait wait;
+
+public BasePage(WebDriver driver) {
+    this.driver = driver;
+    wait = new WebDriverWait(driver, TIMEOUT, POLLING);
+    PageFactory.initElements(new AjaxElementLocatorFactory(driver, TIMEOUT), this);
+    }
+
+protected void waitForElementToAppear(By locator) {
+    wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
 
 
 }
