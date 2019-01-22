ae554851a60786c23f6a22c58d52e2bb37cb6d5f
Stavros Michelis <stavros.michelis@rinftech.com>
2019-01-22 13:34:13 +0200

    .

--- Automation/src/test/java/FirstTest.java
+++ Automation/src/test/java/FirstTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class FirstTest {
 
     @Test
-    public void firstTestMethod(){
+    public void firstTestMethod() {
         WebDriver driver = new ChromeDriver();
 
         //Open Dice.com
@@ -13,8 +13,16 @@ public class FirstTest {
         System.out.println("Dice Opened. Test passed!");
 
         driver.quit();
+    }
+    @Test
+    public void secondTestMethod(){
+        WebDriver driver = new ChromeDriver();
 
+        //Open LinkedIn.com
+        driver.get("https://www.linkedin.com");
+        System.out.println("LinkedIn Opened. Test passed!");
 
+        driver.quit();
 
 
 
