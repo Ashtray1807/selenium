c148bb50008980121532126c6f79e665cc7a7eef
Stavros Michelis <stavros.michelis@rinftech.com>
2019-01-23 11:08:10 +0200

    .

---
+++ Automation/src/test/java/LoginTest.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void positiveLoginTest(){
+
+        //Open Dice login page
+        //Fill email and password
+        //Click sign in and wait for profile page to load
+        //Verifications
+        //-Verify title of the page is Seeker Dashboard-Profile
+        //-Verify correct name on profile page
+
+
+
+    }
+
+
+}
